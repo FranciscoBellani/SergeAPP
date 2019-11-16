c067c9eec51a349f905ef7b5c621ba7eab985d62
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-15 22:05:58 -0300

    issue #1:Se creo componente Habitaciones
      Se creo el componente de Habitaciones.

---
+++ src/app/components/habitaciones/habitaciones.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {HabitacionService} from './../../services/habitacion.service'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-habitaciones',
+  templateUrl: './habitaciones.component.html',
+  styleUrls: ['./habitaciones.component.scss'],
+})
+export class HabitacionesComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder, private habitacionService: HabitacionService) { }
+
+  habitacionForm: FormGroup;
+
+
+
+  ngOnInit() {
+    this.habitacionForm = this.formBuilder.group({
+      nombre: new FormControl('', Validators.required),
+      ubicacion: new FormControl('', Validators.required),
+      categoria:new FormControl(''),
+  });
+  }
+
+  sendData(){
+    const data = this.habitacionForm.getRawValue();
+    this.habitacionService.guardarHabitacion(data).then(habitacion => {
+        debugger;
+    });
+
+  }
+
+}
