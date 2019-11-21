e3b8c7a72684520d5ebc5bfa22babcdad5a8f239
cintiaMillacura2207 <cg2207m@gmail.com>
2019-11-21 16:01:32 -0300

    Issue #9:Se agrego metodo
      Se agrego metodo y url.

--- src/app/services/habitacion.service.ts
+++ src/app/services/habitacion.service.ts
@@ -1,36 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import {HabitacionesComponent} from './../components/habitaciones/habitaciones.component';
+import { HabitacionesComponent } from './../components/habitaciones/habitaciones.component';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HabitacionService {
 
-  urlHabitacion = '';
+    urlHabitacion = 'http://localhost:8080/restful/services/simple.HabitacionMenu/actions/listarHabitaciones/invoke';
 
-  guardarHabitacion(habitacion): Promise<any> {
-      return new Promise((resolve, reject) => {
-          debugger;
-          this.http.post(this.urlHabitacion,habitacion)
-              .subscribe((response: any) => {
-                  resolve(response);
-              }, reject);
-      });
-  }
+    guardarHabitacion(habitacion): Promise<any> {
+        return new Promise((resolve, reject) => {
+            debugger;
+            this.http.post(this.urlHabitacion, habitacion)
+                .subscribe((response: any) => {
+                    resolve(response);
+                }, reject);
+        });
+    }
 
-  buscarHabitacionPorNombre(){
-      
-  }
+    buscarHabitacionPorNombre() {
 
-  listarHabitaciones() {
-      return this.http.get(this.urlHabitacion);
-  }
+    }
 
-  eliminarHabitacion(id:Number): Observable<any> {
-      return this.http.delete(this.urlHabitacion);
-  }
-  
-  constructor(private http: HttpClient) { }
+    listarHabitaciones(): Observable<any> {
+        // return new Promise((resolve, reject) => {
+        debugger;
+        return this.http.get<any>(this.urlHabitacion);
+        //     this.http.get(this.urlHabitacion).subscribe(data => {
+        //         debugger;
+        //         let pepe = data;
+        //         resolve(data);
+        //     });
+        // })
+
+    }
+
+    eliminarHabitacion(id: Number): Observable<any> {
+        return this.http.delete(this.urlHabitacion);
+    }
+
+    constructor(private http: HttpClient) { }
 }
